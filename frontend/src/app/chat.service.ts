1735ea95c5ceeebe07552ff905d7b83ac7df2004
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-05 05:02:30 +0100

    Update chat.service.ts

--- frontend/src/app/chat.service.ts
+++ frontend/src/app/chat.service.ts
@@ -1,45 +1,41 @@
-import { Injectable } from '@angular/core';
-import { io, Socket } from 'socket.io-client';
-import { environment } from './app.config';
+import { Injectable } from "@angular/core";
+import { io, Socket } from "socket.io-client";
+import { environment } from "./app.config";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ChatService {
   private socket: Socket;
 
   constructor() {
-    const token = JSON.parse(localStorage.getItem('authToken') || 'null');
+    const token = JSON.parse(sessionStorage.getItem("authToken") || "null");
 
     this.socket = io(environment.socketUrl, {
-      query: { token: token.token },
+      query: { token },
     });
   }
 
   join(currentUser: string): void {
-    this.socket.emit('join', currentUser);
+    this.socket.emit("join", currentUser);
   }
 
   onOnlineUsers(callback: (onlineUsers: string[]) => void): void {
-    this.socket.on('onlineUsers', (onlineUsers) => callback(onlineUsers));
+    this.socket.on("onlineUsers", (onlineUsers) => callback(onlineUsers));
   }
 
   sendMessage(message: { sender: string; content: string }): void {
-    this.socket.emit('sendMessage', message);
+    this.socket.emit("sendMessage", message);
   }
 
-  onNewMessage(
-    callback: (message: {
-      sender: string;
-      receiver: string;
-      content: string;
-    }) => void
-  ): void {
-    this.socket.on('newMessage', callback);
+  onNewMessage(callback: (message: { sender: string; receiver: string; content: string }) => void): void {
+    this.socket.on("newMessage", callback);
   }
 
-  connect(callback: () => void): void {
-    this.socket.emit('connection', callback);
+  connect(): void {
+    this.socket.on("connect", () => {
+      console.log("Verbindung hergestellt!");
+    });
   }
 
   disconnect(): void {
