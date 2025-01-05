261bc388a3146eee7c8fbf04427149b358a26aef
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-05 05:02:35 +0100

    Update login.component.ts

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,28 +1,22 @@
-import { Router, RouterModule } from '@angular/router';
-import { AuthService } from '../auth.service';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router, RouterModule } from "@angular/router";
+import { AuthService } from "../auth.service";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @Component({
-  selector: 'app-login',
-  imports: [
-    FormsModule,
-    CommonModule,
-    RouterModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  imports: [FormsModule, CommonModule, RouterModule, MatFormFieldModule, MatInputModule],
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
+  email: string = "";
+  password: string = "";
+  errorMessage: string = "";
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -33,9 +27,10 @@ export class LoginComponent {
     };
 
     this.authService.login(loginData).subscribe({
-      next: (token) => {
-        localStorage.setItem('authToken', JSON.stringify(token));
-        this.router.navigate(['/chat']);
+      next: (response) => {
+        sessionStorage.setItem("authToken", JSON.stringify(response.token));
+        this.authService.setUserData(response.user);
+        this.router.navigate(["/chat"]);
       },
       error: (err) => {
         this.errorMessage = `Fehler beim Login: ${err.message}`;
